64216db088f38a7a8c0e67d9168ac12b7871c9f9
thiagogenez <thiagogenez@aefd928c-4250-0410-b366-03211d304494>
2008-06-22 15:10:23 +0000

    add folder etc and sources

---
+++ PostMachine/etc/Arquivo.cpp
@@ -0,0 +1,65 @@
+#include "Arquivo.h"
+
+/* @resume:construtor */
+Arquivo::Arquivo() {
+}
+
+void Arquivo::setEnd(char *enderecoArquivo) {
+	this->enderecoArquivo = enderecoArquivo;
+}
+/* @resume:destrutor */
+Arquivo::~Arquivo() {
+}
+
+/* 
+ * @resume:funcao para abrir o arquivo 
+ * @param: ios_base::openmode mode: modo de abertura
+ */
+void Arquivo::abrir(ios_base::openmode mode) {
+	this->file.open(this->enderecoArquivo, mode);
+}
+
+/* 
+ * @resume:funcao para fechar o arquivo 
+ */
+void Arquivo::fechar() {
+	this->file.close();
+}
+
+/* 
+ * @resume:funcao escrever no arquivo
+ * @param: string line: string que contem
+ */
+void Arquivo::write(string line) {
+	this->file << line;
+}
+
+/* 
+ * @resume:funcao ler do arquivo
+ * @return: linha do arquivo
+ */
+std::string Arquivo::getLinha() {
+	std::string line;
+	getline(this->file, line);
+	return (line);
+}
+
+/* 
+ * @resume:verificar se eh fim do arquivo
+ * @return:
+ * 		   True: se for fim;
+ * 		   False: se nao
+ */
+bool Arquivo::isTheEnd() {
+	return (this->file.eof());
+}
+
+/* 
+ * @resume:verificar se o arquivo esta aberto
+ * @return:
+ * 		   True: se aberto;
+ * 		   False: se nao
+ */
+bool Arquivo::isOpen() {
+	return (this->file.is_open());
+}
