deb2b02206fc6fd8316e015d3a11e63f3949c2f1
thiagogenez <thiagogenez@aefd928c-4250-0410-b366-03211d304494>
2008-07-09 04:32:28 +0000

    colocando a funcao sobrecarga operator Entrada neste cpp

--- trunk/OneStackMachine/Entrada.cpp
+++ trunk/OneStackMachine/Entrada.cpp
@@ -10,15 +10,15 @@ Entrada::Entrada(string estado, string head, string headStackOne) {
 	this->setEstado(estado);
 	this->setHead(head);
 	this->headStack = vector<string>(1);
-	this->setHeadStack(0,headStackOne);
+	this->setHeadStack(0, headStackOne);
 }
 
-void Entrada::setHeadStack(int stack,string headStackOne){
+void Entrada::setHeadStack(int stack, string headStackOne) {
 	this->headStack[stack] = headStackOne;
 }
 
-string Entrada::getHeadStack(int stack) const{
-	return(this->headStack[stack]);
+string Entrada::getHeadStack(int stack) const {
+	return (this->headStack[stack]);
 }
 
 void Entrada::setEstado(string estado) {
@@ -35,4 +35,23 @@ string Entrada::getEstado() const {
 
 string Entrada::getHead() const {
 	return (this->head);
+}
+bool operator==(const Entrada& e1, const Entrada& e2) {
+	return ((e1.estado==e2.estado)&&(e1.head==e2.head)&&(e1.headStack[0]
+			==e2.headStack[0]));
+}
+
+bool operator<(const Entrada& e1, const Entrada& e2) {
+	if (e1.estado<e2.estado)
+	return true;
+	else {
+		if ((e1.estado==e2.estado)&&(e1.head<e2.head)&&(e1.headStack[0]
+						==e2.headStack[0]))
+		return true;
+		if ((e1.estado==e2.estado)&&(e1.head==e2.head)&&(e1.headStack[0]
+						<e2.headStack[0]))
+		return true;
+
+	}
+	return false;
 }
